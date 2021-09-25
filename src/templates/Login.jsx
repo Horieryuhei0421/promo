ad5af534bdeaa87fa7fafbcb003c75cb779d06c9
Horieryuhei0421 <s1f101903478@iniad.org>
2021-09-25 20:31:00 +0900

    reducksのディレクトリ構築

---
+++ src/templates/Login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import { signInAction } from "../reducks/users/actions";
+
+const Login = () => {
+  const dispach = useDispatch();
+  return (
+    <div>
+      <h2>ログイン</h2>
+      <button
+        onClick={() => {
+          dispach(signInAction({ uid: "0001" }));
+          dispach(push("/"));
+        }}
+      >
+        ログインする
+      </button>
+    </div>
+  );
+};
+export default Login;
