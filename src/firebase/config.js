db7b4ac048b437806acb7ae3854d54bc9b3506ed
Horieryuhei0421 <s1f101903478@iniad.org>
2021-09-26 13:54:12 +0900

    サインアップ・イン機能

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0Kqouij4Fz8SP59AePcFJGTWfKHqirp0",
+  authDomain: "promo-e4daf.firebaseapp.com",
+  projectId: "promo-e4daf",
+  storageBucket: "promo-e4daf.appspot.com",
+  messagingSenderId: "33060395241",
+  appId: "1:33060395241:web:ad91844095067e988e7878",
+  measurementId: "G-V272JRFFVZ"
+};
